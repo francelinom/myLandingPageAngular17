c8bd28a8cbf6054a0ab0d410c03ff94f3834118a
Francelino <francelino.silva.prestserv@petrobras.com.br>
2024-02-22 15:42:37 -0300

    Feat: projeto angular 17

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,16 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
+import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
+import { NewsletterFormComponent } from "../newsletter-form/newsletter-form.component";
+
+@Component({
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss',
+    imports: [NgOptimizedImage, HeaderComponent, BtnPrimaryComponent, NewsletterFormComponent]
+})
+export class HomeComponent {
+
+}
