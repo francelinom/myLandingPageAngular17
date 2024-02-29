f688d3b296e75e34bee20941b3ccc20132cf6768
Francelino <francelino.silva.prestserv@petrobras.com.br>
2024-02-29 18:16:36 -0300

    Feat: finalizando lista e card de pokemon

--- src/app/components/pokemon-card/pokemon-card.component.ts
+++ src/app/components/pokemon-card/pokemon-card.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-pokemon-card',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './pokemon-card.component.html',
   styleUrl: './pokemon-card.component.scss'
 })
 export class PokemonCardComponent {
 
+  @Input()
+  pokemon: any;
+
+  @Input()
+  id!: number;
+
+  getImagePokemon() {
+    const numeroFormatado = this.id.toString().padStart(3, '0');
+    return `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${numeroFormatado}.png`;
+  }
 }
